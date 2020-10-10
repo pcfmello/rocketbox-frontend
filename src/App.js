dd4eb40cd588f92780f913105c4879f09ce7c8c3
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2020-10-10 20:19:06 -0300

    Creating new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Routes from './routes'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
-}
+const App = () => <Routes />
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
 import './styles.css'
+import logo from '../../assets/logo.jpeg'
+import API from '../../services/api'
 
 export default class Main extends Component {
+
+    state = {
+        newBox: ''
+    }
+
+    handleSubmit =  async e => {
+        e.preventDefault();
+        
+        const response = await API.post('/boxes', {
+            title: this.state.newBox
+        })
+
+        this.props.history.push(`/box/${response.data._id}`)
+    }
+
+    handleInputChange = e => {
+        this.setState({ newBox: e.target.value })
+    }
+
     render() {
-        // Parado em 20:39m
+        return (
+            <div id="main-container">
+                <form onSubmit={this.handleSubmit}>
+                    <img src={logo} alt="" />
+                    <div className="fields">
+                        <input placeholder="Criar um box" onChange={this.handleInputChange}/>
+                        <button type="submit">Criar</button>
+                    </div>
+                </form>
+            </div>
+        )
     };
 }
